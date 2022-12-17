a4b3168276760ddb45848662f3e19c0960f5174c
Ankit <ankitcool4370@gmail.com>
2022-12-17 21:01:33 +0530

    Performing UC Case 1

---
+++ MS-TesttUserRegSysTests/UserDetailTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MS_TsetUserRegexSys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS_TsetUserRegexSys.Tests
+{
+    [TestClass()]
+    public class UserDetailTests
+    {
+        [TestMethod()]
+        public void FirstNameTest()
+        {
+
+            string userInput = "Abc";
+            bool output = UserDetail.FirstName(userInput);
+
+            Assert.AreEqual(output, true);
+        }
+    }
+}
---
+++ MS-TsetUserRegexSys/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS_TsetUserRegexSys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration using Regex.\n");
+            string userInput = "Abc";
+            UserDetail.FirstName(userInput);
+
+        }
+    }
+}
